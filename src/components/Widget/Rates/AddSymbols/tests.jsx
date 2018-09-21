888ff11e039ee07d9738aab6b21209da024f2c4c
Viktor Pisarenko <xperia.v.j.p@gmail.com>
2018-09-21 21:00:26 +0600

    Add sinon, fix tests for AddSymbols component

--- src/components/Widget/Rates/AddSymbols/tests.jsx
+++ src/components/Widget/Rates/AddSymbols/tests.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 
 import AddSymbols from '.';
 import fixtures from './fixtures';
 
-const MockGenerator = jest.fn();
+const simulateItemClick = (wrapper, itemIndex) => {
+  const itemButton = wrapper.find('.dropdown_item').at(itemIndex).find('button');
+  itemButton.simulate('click', {
+    currentTarget: {
+      dataset: {
+        value: itemButton.props()['data-value'],
+      },
+    },
+  });
+};
 
 describe('AddSymbols Component', () => {
   test('should render with empty symbols object', () => {
@@ -25,7 +35,7 @@ describe('AddSymbols Component', () => {
   });
 
   test('should submit a pair', () => {
-    const onSymbolsSubmit = new MockGenerator();
+    const onSymbolsSubmit = sinon.spy();
 
     const wrapper = shallow(<AddSymbols symbols={fixtures.symbols} onSymbolsSubmit={onSymbolsSubmit} />);
     wrapper.find('.dropdown_label').simulate('click');
@@ -33,13 +43,14 @@ describe('AddSymbols Component', () => {
     expect(wrapper.find('.dropdown_item').length).toEqual(2);
     expect(wrapper.find('.dropdown_item .selected').length).toEqual(0);
 
-    wrapper.find('.dropdown_item').first.simulate('click');
-    expect(wrapper.find('.dropdown_item .selected').length).toEqual(1);
+    simulateItemClick(wrapper, 0);
 
-    wrapper.find('.dropdown_item').last.simulate('click');
+    expect(wrapper.find('.selected').length).toEqual(1);
+
+    simulateItemClick(wrapper, 1);
     expect(wrapper.state('isOpened')).toBe(false);
 
-    const [args] = onSymbolsSubmit.mock;
+    const [args] = onSymbolsSubmit.args;
     expect(args[0]).toEqual(fixtures.symbols.USD.id);
     expect(args[1]).toEqual(fixtures.symbols.GBP.id);
 
@@ -51,13 +62,13 @@ describe('AddSymbols Component', () => {
     wrapper.find('.dropdown_label').simulate('click');
 
     expect(wrapper.find('.dropdown_item').length).toEqual(2);
-    expect(wrapper.find('.dropdown_item .selected').length).toEqual(0);
+    expect(wrapper.find('.selected').length).toEqual(0);
 
-    wrapper.find('.dropdown_item').first.simulate('click');
-    expect(wrapper.find('.dropdown_item .selected').length).toEqual(1);
+    simulateItemClick(wrapper, 0);
+    expect(wrapper.find('.selected').length).toEqual(1);
 
-    wrapper.find('.dropdown_item').first.simulate('click');
-    expect(wrapper.find('.dropdown_item .selected').length).toEqual(0);
+    simulateItemClick(wrapper, 0);
+    expect(wrapper.find('.selected').length).toEqual(0);
 
     wrapper.unmount();
   });
